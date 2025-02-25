4303e885921087cc0fa1449aa1756953f9cdcc23
UmeenHashel <umeenbg16@gmail.com>
2025-02-25 14:57:07 +0530

    Added uppercase and lowercase letters and exclude similar characters

--- app.py
+++ app.py
@@ -15,13 +15,31 @@ def generatePassword():
     length = int(request.args.get('length', 8))
     numbers = request.args.get('numbers', 'false').lower() == 'true'
     symbols = request.args.get('symbols', 'false').lower() == 'true'
+    uppercase = request.args.get('uppercase', 'true').lower() == 'true'
+    lowercase = request.args.get('lowercase', 'true').lower() == 'true'
+    similar = request.args.get('similar', 'false').lower() == 'true'
 
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    numbers = string.digits
+    symbols = string.punctuation
 
-    alpahabet = string.ascii_letters
-    if numbers == True:
+    alpahabet = ''
+    if lowercase:
+        alpahabet += string.ascii_lowercase
+    if uppercase:
+        alpahabet += string.ascii_uppercase
+    if numbers:
         alpahabet += string.digits
-    if symbols == True:
+    if symbols:
         alpahabet += string.punctuation
+
+    if similar:
+        similar_characters = 'il1Lo0O'
+        alphabet = ''.join([char for char in alphabet if char not in similar_characters])
+
+    if not alpahabet:
+        return jsonify({"error": "You must select at least one character type"}) , 400
 
     password = "".join(secrets.choice(alpahabet)for i in range(length))
     return jsonify({"Password": password})
